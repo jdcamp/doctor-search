cdd6b75856c30ac6eb550e63ed701f2c9043eda5
jdc <campajake@gmail.com>
2017-03-17 10:16:51 -0700

    create callback function to dislpay doctors

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,10 +1,13 @@
 var Doctor = require('./../js/doctor.js');
-
+var displayResults = function(doctors) {
+  console.log(doctors.data[0].profile.bio);
+    $('.results').append(doctors.data[0].profile.bio);
+};
 $(document).ready(function() {
     $('.search-form').submit(function(e) {
         e.preventDefault();
         var medicalIssue = $('input[name="name"]').val()
-        Doctor.getDoctors(medicalIssue);
+        Doctor.getDoctors(medicalIssue, displayResults) ;
     });
 
 });
